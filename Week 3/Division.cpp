ba2c01578f104073224d8c9555843b9a1f634a73
GritzMaze <jivkovvesselin@gmail.com>
2020-10-24 00:50:33 +0300

    Division by?

--- Week 3/Division.cpp
+++ Week 3/Division.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int main() {
-cout << "Division by? \n";
-int n = 0;
-cin >> n;
-if(n > 0 && n <= 100){
-for(int i = 1; i <= 100; i++)
-{
-  if(i % n == 0)
-  {
-    cout << i <<" ";
-  }
-}
-} else cout << "Invalid input";
-  return 0;
+        cout << "Division by? \n";
+        int n = 0;
+        cin >> n;
+        if(n > 0 && n <= 100) {
+                for(int i = 1; i <= 100; i++)
+                {
+                        if(i % n == 0)
+                        {
+                                cout << i <<" ";
+                        }
+                }
+        } else cout << "Invalid input";
+        return 0; 
 }
