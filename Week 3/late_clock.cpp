f08c0dec9f4bf09e08292563b4528a4f4a75dd2c
GritzMaze <jivkovvesselin@gmail.com>
2020-10-24 00:49:46 +0300

    Late clock

--- Week 3/late_clock.cpp
+++ Week 3/late_clock.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int main() {
-int hour = 0;
-int minutes = 0;
-int diff = 0;
-cout << "Late clock" << endl;
-cin >> hour >> minutes;
-if(minutes >= 0 && minutes <=60) {
-if((minutes + 30) >= 60) {
- hour++;
- diff = ((minutes + 30) - 60);
- minutes = 0 + diff;
- cout << hour <<":" << minutes==0? cout << "00" : cout << minutes;
-} else cout << hour << ":" <<cout << minutes + 30;
-} else cout << "Invalid input";
+        int hour = 0;
+        int minutes = 0;
+        int diff = 0;
+        cout << "Late clock" << endl;
+        cin >> hour >> minutes;
+        if(minutes >= 0 && minutes <=60) {
+                if((minutes + 30) >= 60) {
+                        hour++;
+                        diff = ((minutes + 30) - 60);
+                        minutes = 0 + diff;
+                        cout << hour <<":" << minutes==0? cout << "00" : cout << minutes;
+                } else cout << hour << ":" <<cout << minutes + 30;
+        } else cout << "Invalid input";
 
-return 0;
+        return 0;
 }
