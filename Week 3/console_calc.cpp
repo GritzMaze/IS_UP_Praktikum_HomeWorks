efb705438ba9c5e8da1cc13305aca3c551a47068
GritzMaze <jivkovvesselin@gmail.com>
2020-10-24 00:48:38 +0300

    Console Calculator

--- Week 3/console_calc.cpp
+++ Week 3/console_calc.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 
 int main() {
-double a = 0, b = 0;
-char sym;
-cout << "Console calculator" << endl;
-cin >> a >> sym >> b;
-switch(sym) {
-    case '+':
-        cout << a + b;
-        break;
-    case '-':
-        cout << a - b;
-        break;
-    case '/':
-        cout << a / b;
-        break;
-    case '*':
-        cout << a * b;
-        break;
-    default:
-        cout << "Calculator can't recognise symbol.";
-}
-return 0;
+        double a = 0, b = 0;
+        char sym;
+        cout << "Console calculator" << endl;
+        cin >> a >> sym >> b;
+        switch(sym) {
+        case '+':
+                cout << a + b;
+                break;
+        case '-':
+                cout << a - b;
+                break;
+        case '/':
+                cout << a / b;
+                break;
+        case '*':
+                cout << a * b;
+                break;
+        default:
+                cout << "Calculator can't recognise symbol.";
+        }
+        return 0;
 }
