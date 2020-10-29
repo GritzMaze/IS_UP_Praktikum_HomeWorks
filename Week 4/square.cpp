b65e3a5e1d93f1d7d4ea68ce7155e810a57dd2ad
GritzMaze <jivkovvesselin@gmail.com>
2020-10-29 18:35:35 +0200

    Some fixes

--- Week 4/square.cpp
+++ Week 4/square.cpp
@@ -4,12 +4,13 @@ int main() {
 std::cout << "Square \n";
 int n = 0;
 std::cin >> n;
-for(int col = 0; col < n - 1; col++) {
-  std::cout << " * ";
-  for(int row = 1; row < n - 2; row++) {
-    std::cout << " ";
+for(int col = 0; col < n; col++) {
+  for(int row = 0; row < n; row++) {
+    if(col == 0 || col == n - 1 || row == n - 1 || row == 0) std::cout << " * ";
+    else if(col < row || col == row) std::cout << "   ";
+    else std::cout << " * ";
   }
-std::cout << " *  \n";
+std::cout << "\n";
 }
 
   return 0;
--- Week 4/threeIsThree.cpp
+++ Week 4/threeIsThree.cpp
@@ -4,13 +4,13 @@ int main() {
         int n = 0, digit = 0, sum = 1;
         std::cout << "3 is 3 \n";
         std:: cin >> n;
-        if((n >= 100 && n <= 999) && (n >= -999 && n <= -100))
+        if((n >= 100 && n <= 999) || (n >= -999 && n <= -100))
         {
                 while(n) {
                         sum *= n % 10;
                         n = n / 10;
                 }
-                if((sum >= 100 && sum <= 999) && (sum >= -999 && sum <= -100)) {
+                if((sum >= 100 && sum <= 999) || (sum >= -999 && sum <= -100)) {
                         std::cout << "YES";
                 } else std::cout << "No";
         } else std::cout << "Not a three digit number or NaN";
