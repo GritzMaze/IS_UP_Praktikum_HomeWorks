295b867734fa8ee17f40b84b9f88e33088419aeb
GritzMaze <jivkovvesselin@gmail.com>
2020-11-02 20:58:01 +0200

    One month session

---
+++ Homework_1/One_month_session.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+
+bool isLeap(int year);
+bool isPrime(int num);
+
+int main() {
+        std::cout << "One month session \n";
+        int year = 0;
+        int days = 0;
+        double sum = 0.0;
+        std::cin >> year;
+        if(isLeap(year)) days = 29;
+        else days = 28;
+        for(int i = 1; i <= days; i++)
+        {
+                if(i % 2 == 0) sum += 2.99;
+                else if(isPrime(i)) sum += 2.00;
+                else sum += 3.00;
+                std::cout << sum << std::endl;
+        }
+        double answer = sum /days;
+        std::cout << std::round(answer * 100.0) / 100.0;
+
+
+        return 0;
+}
+
+bool isLeap(int year) {
+        if (year % 4 == 0)
+        {
+                if (year % 100 == 0)
+                {
+                        if (year % 400 == 0) return true;
+                        else return false;
+                } else return true;
+        }
+        else return false;
+}
+
+bool isPrime(int num) {
+        if(num == 1) return false;
+        for(int i = 2; i <= num / 2; i++)
+        {
+                if(num % i == 0) return false;
+        }
+        return true;
+}
