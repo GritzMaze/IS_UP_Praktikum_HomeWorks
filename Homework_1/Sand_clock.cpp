93bd37d7072b0ef1a2f781c4feb9789b7d668bbe
GritzMaze <jivkovvesselin@gmail.com>
2020-11-03 23:08:40 +0200

    Sand clock

--- Homework_1/Sand_clock.cpp
+++ Homework_1/Sand_clock.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 
 int main() {
-std::cout << "Sand Clock \n";
-int n = 0;
-std::cin >> n;
-for(int rows = 0; rows < n; rows++)
-{
-  for(int col = 0; col < n; col++) {
-    if(rows == 0 || rows == n - 1) std::cout << "*";
-    else if(col == rows || (n - 1) - rows == col) std::cout << "*";
-    else std::cout << " ";
-  }
-  std::cout << std::endl;
-}
+        std::cout << "Sand Clock \n";
+        int n = 0;
+        std::cin >> n;
+        for(int rows = 0; rows < n; rows++)
+        {
+                for(int col = 0; col < n; col++) {
+                        if(rows == 0 || rows == n - 1) std::cout << "*";
+                        else if(col == rows || (n - 1) - rows == col) std::cout << "*";
+                        else std::cout << " ";
+                }
+                std::cout << std::endl;
+        }
 
-  return 0;
+        return 0;
 }
