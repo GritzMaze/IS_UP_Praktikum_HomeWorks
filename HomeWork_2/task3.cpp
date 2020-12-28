7b241a94e36b07a2273876599e94886966950b1f
GritzMaze <>
2020-12-28 15:30:28 +0200

    .

--- HomeWork_2/task3.cpp
+++ HomeWork_2/task3.cpp
@@ -1,40 +1,36 @@
 #include <iostream>
+
 #include <cstring>
 
-int checkLength(char *arr)
-{
+int checkLength(char * arr) {
     int length = 0;
-    while(arr[length] != '\0')
-    {
+    while (arr[length] != '\0') {
         length++;
     }
     return length;
 }
 
-bool isMatch(const char a, const char b)
-{
-    if(a == b) return true;
+bool isMatch(const char a,
+    const char b) {
+    if (a == b) return true;
     else return false;
 }
 
-void toUpperCase(char &letter)
-{
-    if(letter >= 'a' && letter <= 'z')
+void toUpperCase(char & letter) {
+    if (letter >= 'a' && letter <= 'z')
         letter = letter - ('a' - 'A');
 }
 
-void takeString(char *main, char *sec)
-{
-    int length = 0, length2 = 0;
-    length = checkLength(main);
-    length2 = checkLength(sec);
+void takeString(char * main, char * sec) {
+        int length = 0, length2 = 0;
+        length = checkLength(main);
+        length2 = checkLength(sec);
 
-    for(int i = 0; i < length; i++)
-    {
-        if(strchr(sec, main[i])) toUpperCase(main[i]);
-    }
+        for (int i = 0; i < length; i++) {
+            if (strchr(sec, main[i])) toUpperCase(main[i]);
+        }
 
-    /**
+        /**
      * 
      * Boring way and long way ( ͡ʘ ͜ʖ ͡ʘ)
      * 
