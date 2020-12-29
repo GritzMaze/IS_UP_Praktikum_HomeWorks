11b5489849e72fc98adec212de1d2ff515426599
GritzMaze <jivkovvesselin@gmail.com>
2020-12-29 12:44:45 +0200

    tasks

--- HomeWork_2/task.cpp
+++ HomeWork_2/task.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 
 void shift(int rows) {
-    for (int i = 1; i <= rows; i++) {
-        for (int k = i; k <= 9; k++)
-            std::cout << k << " ";
+        for (int i = 1; i <= rows; i++) {
+                for (int k = i; k <= 9; k++)
+                        std::cout << k << " ";
 
-        for (int j = 1; j <= i; j++)
-            std::cout << j << " ";
+                for (int j = 1; j <= i; j++)
+                        std::cout << j << " ";
 
-        std::cout << "\n";
-    }
+                std::cout << "\n";
+        }
 
 
 }
 
 int main() {
-    unsigned int m = 0;
-    std::cin >> m;
-    shift(m);
-    return 0;
+        unsigned int m = 0;
+        std::cin >> m;
+        shift(m);
+        return 0;
 }
--- HomeWork_2/task3.cpp
+++ HomeWork_2/task3.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-
 #include <cstring>
 
 int checkLength(char * arr) {
-    int length = 0;
-    while (arr[length] != '\0') {
-        length++;
-    }
-    return length;
+        int length = 0;
+        while (arr[length] != '\0') {
+                length++;
+        }
+        return length;
 }
 
 bool isMatch(const char a,
-    const char b) {
-    if (a == b) return true;
-    else return false;
+             const char b) {
+        if (a == b) return true;
+        else return false;
 }
 
 void toUpperCase(char & letter) {
-    if (letter >= 'a' && letter <= 'z')
-        letter = letter - ('a' - 'A');
+        if (letter >= 'a' && letter <= 'z')
+                letter = letter - ('a' - 'A');
 }
 
 void takeString(char * main, char * sec) {
@@ -27,38 +26,38 @@ void takeString(char * main, char * sec) {
         length2 = checkLength(sec);
 
         for (int i = 0; i < length; i++) {
-            if (strchr(sec, main[i])) toUpperCase(main[i]);
+                if (strchr(sec, main[i])) toUpperCase(main[i]);
         }
 
         /**
-     * 
-     * Boring way and long way ( ͡ʘ ͜ʖ ͡ʘ)
-     * 
-     **/
+         *
+         * Boring way and long way ( ͡ʘ ͜ʖ ͡ʘ)
+         *
+         **/
 
-    // for(int i = 0; i < length; i++) {
-    //     for(int j = 0; j < length2; j++)
-    //     {
-    //         if(isMatch(main[i], sec[j])){
-    //             toUpperCase(main[i]);
-    //             break;
-    //         }
-    //     }
+        // for(int i = 0; i < length; i++) {
+        //     for(int j = 0; j < length2; j++)
+        //     {
+        //         if(isMatch(main[i], sec[j])){
+        //             toUpperCase(main[i]);
+        //             break;
+        //         }
+        //     }
 
-    // }
+        // }
 
-    for(int i = 0; i < length; i++)
-    std::cout << main[i];
+        for(int i = 0; i < length; i++)
+                std::cout << main[i];
 
 }
 
 
 int main() {
-    char arr[256], arr2[256];
-    std::cin.getline(arr, 256);
-    std::cin.getline(arr2, 256);
+        char arr[256], arr2[256];
+        std::cin.getline(arr, 256);
+        std::cin.getline(arr2, 256);
 
-    takeString(arr, arr2);
+        takeString(arr, arr2);
 
-    return 0;
+        return 0;
 }
